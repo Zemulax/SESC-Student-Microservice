bfe33fe33cccf1b7f1ddcab77fdd4fc81ae71244
Zemulax <100323567+Zemulax@users.noreply.github.com>
2025-04-07 19:53:09 +0100

    config file

---
+++ student-service/src/main/java/com/sesc/unistudycircle/student_service/services/Configuration.java
@@ -0,0 +1,16 @@
+package com.sesc.unistudycircle.student_service.services;
+
+import org.apache.hc.core5.http.HttpHost;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+//                .baseUrl("http://api-gateway:8080")
+                .build();
+    }
+}
